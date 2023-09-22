4d99ff667461d44c1dd4aeef48d60c8b59406055
MailsonPrates <mailsonprates@gmail.com>
2023-09-22 11:24:17 -0300

    Add mais itens

--- examples/routes/completo/components/template/sidebar.js
+++ examples/routes/completo/components/template/sidebar.js
@@ -2,61 +2,73 @@ import { Link } from "../../../../../src/core/router/jeact-router.js";
 
 export default function Sidebar(props={}){
 
+    let elements = [
+        $("<li>", {
+            class: "list-group-item",
+            html: Link({
+                href: "/",
+                html: "Home"
+            })
+        }),
+        $("<li>", {
+            class: "list-group-item",
+            html: Link({
+                href: "/category/category-slug",
+                html: "Categoria"
+            })
+        }),
+        $("<li>", {
+            class: "list-group-item",
+            html: Link({
+                href: "/pedidos",
+                html: "Pedidos"
+            })
+        }),
+        $("<li>", {
+            class: "list-group-item",
+            html: Link({
+                href: "/pedidos/filter",
+                html: "-- Pedidos filtro"
+            })
+        }),
+        $("<li>", {
+            class: "list-group-item",
+            html: Link({
+                href: "/pedidos/123",
+                html: "-- Pedido 123"
+            })
+        }),
+        $("<li>", {
+            class: "list-group-item",
+            html: Link({
+                href: "/pedidos/123/imprimir",
+                html: "---- Imprimir pedido 123"
+            })
+        }),
+        $("<li>", {
+            class: "list-group-item",
+            html: Link({
+                href: "/contact",
+                html: "Contato"
+            })
+        })
+    ];
+
+    for ( var i=0; i<300; i++ ){
+        elements.push($("<li>", {
+            class: "list-group-item",
+            html: Link({
+                href: `/pedidos/${i}`,
+                html: `Pedido #${i}`
+            })
+        }));
+    }
+
     return $("<aside>", {
         class: "template-sidebar"+(props.class ? " "+props.class : ""),
         html: $("<ul>", {
             class: "list-group",
-            html: [
-                $("<li>", {
-                    class: "list-group-item",
-                    html: Link({
-                        href: "/",
-                        html: "Home"
-                    })
-                }),
-                $("<li>", {
-                    class: "list-group-item",
-                    html: Link({
-                        href: "/category/category-slug",
-                        html: "Categoria"
-                    })
-                }),
-                $("<li>", {
-                    class: "list-group-item",
-                    html: Link({
-                        href: "/pedidos",
-                        html: "Pedidos"
-                    })
-                }),
-                $("<li>", {
-                    class: "list-group-item",
-                    html: Link({
-                        href: "/pedidos/filter",
-                        html: "-- Pedidos filtro"
-                    })
-                }),
-                $("<li>", {
-                    class: "list-group-item",
-                    html: Link({
-                        href: "/pedidos/123",
-                        html: "-- Pedido 123"
-                    })
-                }),
-                $("<li>", {
-                    class: "list-group-item",
-                    html: Link({
-                        href: "/pedidos/123/imprimir",
-                        html: "---- Imprimir pedido 123"
-                    })
-                }),
-                $("<li>", {
-                    class: "list-group-item",
-                    html: Link({
-                        href: "/contact",
-                        html: "Contato"
-                    })
-                }),
-            ]
+            html: elements
         })
     })
 
